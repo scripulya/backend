a22dec232bc0b7162bd6e89669a7553bdc9df2d6
scripthound <grinkiko@gmail.com>
2021-10-16 10:42:32 +0300

    [AMEND] amend for previous commit

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from typing import List
 from fastapi import FastAPI
 from fastapi.params import Query
 from fastapi.middleware.cors import CORSMiddleware
-from geojson import FeatureCollection, Feature, Point
+from geojson import FeatureCollection, Feature, Point, MultiPolygon
+from sqlalchemy.sql.sqltypes import Integer
 from sqlalchemy.future import select
 from sqlalchemy import func
 
-from core.models import RentalProperty
+from core.models import RentalProperty, Municipality
 from setup import async_session
 
 app = FastAPI()
@@ -25,12 +26,32 @@ app.add_middleware(
 )
 
 
+def get_median_apartments_price(bbox: list):
+    regions = select(Municipality).where(
+        func.ST_Intersects(
+            Municipality.geom,
+            func.ST_MakeEnvelope(*bbox, 4326))).cte("bboxed")
+    prices = select(
+        regions.c.name,
+        regions.c.geom.ST_AsGeoJSON().label("geom"),
+        func.count(RentalProperty.coords).label("aparts_count"),
+        func.round(
+            func.percentile_cont(0.5)\
+                .within_group(
+                    func.regexp_replace(
+                        RentalProperty.price,
+                        r'\D+',
+                        '',
+                        'g').cast(Integer))))
+    prices = prices.join(
+            RentalProperty,
+            func.ST_Intersects(regions.c.geom, RentalProperty.coords))
+    prices = prices.group_by(regions.c.name, regions.c.geom)
+    return prices
+
+
 def get_geojson_points_by_bbox(bbox: list):
-    points = select(
-        RentalProperty.address,
-        RentalProperty.resource_link,
-        RentalProperty.price,
-        RentalProperty.coords)\
+    points = select(RentalProperty)\
             .cte("rental_coords")
     points = select(
         points.c.resource_link,
@@ -41,6 +62,28 @@ def get_geojson_points_by_bbox(bbox: list):
             points.c.coords.ST_Intersects(
                 func.ST_MakeEnvelope(*bbox, 4326)))
     return points
+
+
+@app.get('/get_rent_count_by_municipality/')
+async def get_municipalities(bbox: List[float] = Query(ESTONIA_BBOX)):
+    message = []
+    async with async_session() as session:
+        prices = get_median_apartments_price(bbox)
+        result = await session.execute(prices)
+        for name, geom, count, median in result:
+            coords = json.loads(geom)
+            coords = coords['coordinates']
+            multipolygon = MultiPolygon(coords)
+
+            properties = {
+                "municipality_name": name,
+                "aparts_count": count,
+                "price_median": median
+            }
+            feature = Feature(geometry=multipolygon, properties=properties)
+            message.append(feature)
+        message = FeatureCollection(message)
+        return message
 
 
 @app.get('/get_locations/')
